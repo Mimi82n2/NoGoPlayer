c9b5511a9237b0f33a6c82fb6ccbc237f9376b5e
Che-Ming Chung <cheming@ualberta.ca>
2022-11-30 21:24:15 -0700

    board_util

--- nogo4/board_util.py
+++ nogo4/board_util.py
@@ -5,24 +5,59 @@ Utility functions for Go board.
 
 import numpy as np
 import random
+from board_base import EMPTY, BLACK, WHITE, BORDER
 from typing import List
-from board_base import GO_COLOR, GO_POINT
-from board import GoBoard
+
+def is_black_white(color):
+    return color == BLACK or color == WHITE
+
+
+def is_black_white_empty(color):
+    return color == BLACK or color == WHITE or color == EMPTY
+
+
+"""
+A GO_POINT is a point on a Go board.
+It is encoded as a 32-bit integer, using the numpy type.
+"""
+GO_POINT = np.int32
+
+"""
+Encoding of special pass move
+"""
+PASS = None
+
+"""
+Encoding of "not a real point", used as a marker
+"""
+NULLPOINT = 0
+
+"""
+The largest board we allow.
+To support larger boards the coordinate printing in
+GtpConnection.format_point needs to be changed.
+"""
+MAXSIZE = 25
+
+"""
+where1d: Helper function for using np.where with 1-d arrays.
+The result of np.where is a tuple which contains the indices
+of elements that fulfill the condition.
+For 1-d arrays, this is a singleton tuple.
+The [0] indexing is needed to extract the result from the singleton tuple.
+"""
+def where1d(condition):
+    return np.where(condition)[0]
+
+
+def coord_to_point(row, col, boardsize):
+    NS = boardsize + 1
+    return NS * row + col
+
 
 class GoBoardUtil(object):
     @staticmethod
-    def generate_legal_moves(board: GoBoard, color: GO_COLOR) -> List:
-        """
-        generate a list of all legal moves on the board.
-        Does not include the Pass move.
-
-        Arguments
-        ---------
-        board:
-            a GoBoard
-        color:
-            the color to generate the move for.
-        """
+    def generate_legal_moves(board, color):
         moves: np.ndarray[GO_POINT] = board.get_empty_points()
         legal_moves: List[GO_POINT] = []
         
@@ -30,15 +65,13 @@ class GoBoardUtil(object):
             if board.is_legal(move, color):
                 legal_moves.append(move)
         return legal_moves
-        
-        
 
     @staticmethod
-    def generate_random_move(board: GoBoard, color: GO_COLOR, 
-                             use_eye_filter: bool) -> GO_POINT:
+    def generate_random_move(board, color):
         """
         Generate a random move.
-    
+        Return PASS if no move found
+
         Arguments
         ---------
         board : np.array
@@ -46,50 +79,34 @@ class GoBoardUtil(object):
         color : BLACK, WHITE
             the color to generate the move for.
         """
-        moves: np.ndarray[GO_POINT] = board.get_empty_points()
-        np.random.shuffle(moves)
-        for move in moves:
-            legal: bool = not (
-                use_eye_filter and board.is_eye(move, color)
-            ) and board.is_legal(move, color)
-            if legal:
-                return move
-        
-        
+        moves = GoBoardUtil.generate_legal_moves(board, color)
+        if (len(moves) == 0):
+            return None
+        # choose one legal move randomly
+        return random.choice(moves)
 
     @staticmethod
-    def generate_random_moves(board: GoBoard, use_eye_filter: bool) -> List:
+    def generate_random_moves(board, use_eye_filter):
         """
         Return a list of random (legal) moves with eye-filtering.
         """
-        empty_points: np.ndarray[GO_POINT] = board.get_empty_points()
-        color: GO_COLOR = board.current_player
-        moves: List[GO_POINT] = []
-        for move in empty_points:
-            legal: bool = \
-                not (
-                    use_eye_filter and board.is_eye(move, color)
-                ) and board.is_legal(move, color)
-            if legal:
-                moves.append(move)
-        return moves
-        
+
+        color = board.current_player
+        legal_moves = GoBoardUtil.generate_legal_moves(board, color)
+        random.shuffle(legal_moves)
+
+        return legal_moves
 
     @staticmethod
-    def get_twoD_board(go_board: GoBoard) -> np.ndarray:
-        """
-        Return: numpy array
-        a two dimensional numpy array with the goboard.
-        Shows stones and empty points as encoded in board_base.py.
-        Result is not padded with BORDER points.
-        Rows 1..size of goboard are copied into rows 0..size - 1 of board2d
-        Then the board is flipped up-down to be consistent with the
-        coordinate system in GoGui (row 1 at the bottom).
-        """
-        size: int = go_board.size
-        board2d: np.ndarray[GO_POINT] = np.zeros((size, size), dtype=GO_POINT)
+    def opponent(color):
+        return WHITE + BLACK - color
+
+    @staticmethod
+    def get_twoD_board(goboard):
+        size = goboard.size
+        board2d = np.zeros((size, size), dtype=GO_POINT)
         for row in range(size):
-            start: int = go_board.row_start(row + 1)
-            board2d[row, :] = go_board.board[start : start + size]
+            start = goboard.row_start(row + 1)
+            board2d[row, :] = goboard.board[start : start + size]
         board2d = np.flipud(board2d)
         return board2d
